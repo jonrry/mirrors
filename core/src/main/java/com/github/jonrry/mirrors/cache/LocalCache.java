234803976ce4ef20d5d0a2b840d4842d32d57899
zongyue.xzy <xiezongyue0811@gmail.com>
2016-12-27 01:08:18 +0800

    init

--- core/src/main/java/com/github/jonrry/mirrors/cache/LocalCache.java
+++ core/src/main/java/com/github/jonrry/mirrors/cache/LocalCache.java
@@ -1,7 +1,13 @@
 package com.github.jonrry.mirrors.cache;
 
+import com.google.common.cache.CacheBuilder;
+
 /**
  * Created by zongyue.xzy on 16/12/27.
  */
 public class LocalCache {
+
+    public void putValue(){
+        CacheBuilder.newBuilder();
+    }
 }
