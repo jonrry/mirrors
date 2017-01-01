eb47feb5783e3d632ca0aa727e794ad4749c27e2
zongyue.xzy <xiezongyue0811@gmail.com>
2017-01-01 13:29:58 +0800

    add cache

--- core/src/main/java/com/github/jonrry/mirrors/cache/LocalCache.java
+++ core/src/main/java/com/github/jonrry/mirrors/cache/LocalCache.java
@@ -1,13 +1,38 @@
 package com.github.jonrry.mirrors.cache;
 
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+
+import java.util.concurrent.TimeUnit;
 
 /**
- * Created by zongyue.xzy on 16/12/27.
+ * Created by shiru on 16/12/27.
  */
 public class LocalCache {
 
-    public void putValue(){
-        CacheBuilder.newBuilder();
+    //存放value的值
+    public void putValue() {
+        CacheBuilder cacheBuilder = CacheBuilder.newBuilder();
+        cacheBuilder.build(new CacheLoader() {
+            @Override
+            public Object load(Object key) throws Exception {
+                return null;
+            }
+        });
+
+
+        CacheBuilder.newBuilder()
+                .expireAfterAccess(10, TimeUnit.MINUTES)
+                .build(
+                        new CacheLoader<String, String>() {
+                            @Override
+                            public String load(String key) throws Exception {
+                                return "";
+                            }
+                        }
+                );
     }
+
+
+
 }
